8433172eacb8f804685eaae324ded6e3e0cf0dc0
KimDoHyun <dohyun9565@gmail.com>
2019-11-05 20:22:07 +0900

    2019-11-05

---
+++ Heap/PriorityQueue/PriorityQueue.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "UsefulHeap.h"
+
+typedef Heap PQueue;
+typedef HData PQData;
+
+void PQueueInit(PQueue* ppq, PriorityComp pc);
+int PQIsEmpty(PQueue* ppq);
+
+void PEnqueue(PQueue* ppq, PQData data);
+PQData PDequeue(PQueue* ppq);
