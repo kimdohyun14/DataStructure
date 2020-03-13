235d86cd0a82f2b435a91a116d99cb038fae949b
DoHyun Kim <dohyun9565@gmail.com>
2020-03-13 21:29:14 +0900

    문자열 매칭 KMP 알고리즘

---
+++ 공부한것들/SimpleStringMatching.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+char *parent = "ABCDEFG";
+char *pattern = "EF";
+
+int main()
+{
+	int parentSize = strlen(parent);
+	int patternSize = strlen(pattern);
+	for (int i = 0; i <= parentSize - patternSize; i++)
+	{
+		int found = 1;
+		for (int j = 0; j < patternSize; j++)
+		{
+			if (parent[i + j] != pattern[j])
+			{
+				found = 0;
+				break;
+			}
+			if (found)
+			{
+				printf("%d번째에서 찾았습니다.\n", i + 1);
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
