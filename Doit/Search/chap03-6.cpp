798fe1beed4e5935b9fa74c55fcdec3b03a60e84
Kim Do Hyun <dohyun9565@gmail.com>
2020-07-27 19:32:16 +0900

    function pointer 이용하는 방법

---
+++ Doit/Search/chap03-6.cpp
@@ -0,0 +1,34 @@
+/* µ¡¼À, °ö¼ÀÇ¥ */
+#include <stdio.h>
+
+int sum(int x1, int x2)
+{
+	return x1 + x2;
+}
+
+int mul(int x1, int x2)
+{
+	return x1 * x2;
+}
+
+void kuku(int(*calc)(int, int))
+{
+	int i, j;
+	for (i = 1; i <= 9; ++i)
+	{
+		for (j = 1; j <= 9; ++j)
+		{
+			printf("%3d", (*calc)(i, j));
+		}
+		putchar('\n');
+	}
+}
+
+int main()
+{
+	puts("µ¡¼ÀÇ¥");
+	kuku(sum); // ÇÔ¼öÀÇ ÀÌ¸§Àº ±× ÇÔ¼ö¿¡ ´ëÇÑ Æ÷ÀÎÅÍ¿Í °°´Ù
+	puts("\n °ö¼ÀÇ¥");
+	kuku(mul);
+	return 0;
+}
